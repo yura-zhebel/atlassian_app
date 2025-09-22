ce8cf68f31aeaf89cd01dcb16518a32ed2b60fed
yura-zhebel <yura.zhebel87@gmail.com>
2025-09-22 22:59:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,13 +2,22 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+# Отдаём App descriptor
 @app.route('/atlassian-connect.json')
 def descriptor():
     return send_from_directory('.', 'atlassian-connect.json')
 
+# Отдаём страницу с кнопкой
 @app.route('/index.html')
 def index():
     return send_from_directory('.', 'index.html')
 
+# Проверка, что сервер работает
+@app.route('/status')
+def status():
+    return "OK"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
