0bc771b5cf8009e8db412b2aa78afd5d773eb8e7
yura-zhebel <yura.zhebel87@gmail.com>
2025-09-23 00:08:06 +0200

    Update server.py

--- server.py
+++ server.py
@@ -21,3 +21,5 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+resp.headers['X-Frame-Options'] = 'ALLOWALL'
+resp.headers['Content-Security-Policy'] = "frame-ancestors *"
